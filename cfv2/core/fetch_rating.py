2c008e01f110e19d5ea4d0d0020027a6632ded56
HackerX-47 <omkar121207@gmail.com>
2026-08-28 11:02:58 +0530

    added more fields in rating core

--- cfv2/core/fetch_rating.py
+++ cfv2/core/fetch_rating.py
@@ -4,19 +4,45 @@ from cfv2.display.rating_display import *
 
 def fetch_rating_data(name, opt = 0):
 
-    params = {"handle" : name}
-    data = caller("user.rating", params)
+    params  = {"handle" : name}
+    data    = caller("user.rating", params)
     if data is None:
         return 
+
+    totalContests = len(data)
+    best_rank   = 0
+    worst_rank  = float('inf')
+    avg_rank    = 0
+    avg_rating_chng = 0
+    pos_chng_count  = 0
+    neg_chng_count  = 0
+    no_chng_count   = 0
+    best_rating     = 0
+    worst_rating    = float('inf')
+    ovl_rating_chng = data[-1]["newRating"] - data[0]["oldRating"]
+
+
 
     header();
 
     for curr in data:
 
-        contestName = curr["contestName"]
-        matches = re.findall(r"Div\. ?\d", contestName)
+        contestName  = curr["contestName"]
+        matches      = re.findall(r"Div\. ?\d", contestName)
+        currRating   = curr["newRating"]
         ratingChange = curr["newRating"]-curr["oldRating"]
-        rank = curr["rank"]
+        rank         = curr["rank"]
+
+        best_rank        = max(rank, best_rank)
+        worst_rank       = min(rank, worst_rank)
+        avg_rank        += rank/totalContests
+        avg_rating_chng += ratingChange/totalContests
+        best_rating      = max(currRating, best_rating)
+        worst_rating     = min(currRating, worst_rating)
+
+        if ratingChange > 0:    pos_chng_count += 1
+        if ratingChange < 0:    neg_chng_count += 1
+
 
         if matches: contestType = " + ".join(matches)
         else:       contestType = contestName
@@ -25,14 +51,26 @@ def fetch_rating_data(name, opt = 0):
             "contestType" : contestType,
             "rank" : rank, 
             "ratingChange" : ratingChange,
-            "currRating" : curr["newRating"]
+            "currRating" : currRating
         }
 
         if opt == 1: print1(data1)
 
+    no_chng_count = totalContests - (pos_chng_count + neg_chng_count)
+
     data2 = {
-        "totalContest"  : len(data), 
-        "currRating"    : data[-1]["newRating"]
+        "totalContest"  : totalContests, 
+        "currRating"    : data[-1]["newRating"],
+        "bestRank"      : best_rank,
+        "worstRank"     : worst_rank,
+        "avgRank"       : avg_rank,
+        "avgRatingChng" : avg_rating_chng,
+        "posChngCount"  : pos_chng_count,
+        "negChngCount"  : neg_chng_count,
+        "noChngCount"   : no_chng_count,
+        "bestRating"    : best_rating,
+        "worstRating"   : worst_rating,
+        "ovlRatingChng" : ovl_rating_chng
     }
 
     if opt == 1:
